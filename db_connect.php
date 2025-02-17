7962135b1c5c107592b7a51dedbbb1a6c92f4ddc
Timothy-Obote <obotetimothy005@gmail.com>
2025-02-17 08:40:18 -0800

    creating a database in phpmyadmin to connect my website

---
+++ db_connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost"; // Change if using an external database
+$username = "root"; // Default MySQL username
+$password = ""; // Default password (empty if using XAMPP)
+$dbname = "lost_and_found_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
